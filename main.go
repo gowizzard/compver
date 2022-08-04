c93c1dc22b935d656d3c0076d3f55952cfa87a2b
Jonas Kwiedor <mail@gowizzard.de>
2022-08-04 18:06:07 +0200

    feat: Add main function.

--- main.go
+++ main.go
@@ -1,1 +1,50 @@
-package compver
+package main
+
+import (
+	"fmt"
+	"github.com/gowizzard/compver/version_core"
+	"log"
+	"os"
+)
+
+func main() {
+
+	os.Args = os.Args[:1]
+	if len(os.Args) >= 2 {
+
+		version1, err := version_core.Get(os.Args[1])
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		version2, err := version_core.Get(os.Args[2])
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		var blocks []version_core.Block
+		blocks = append(
+			blocks, version_core.Block{
+				Name:    "major",
+				Number1: version1.Major,
+				Number2: version2.Major,
+			}, version_core.Block{
+				Name:    "minor",
+				Number1: version1.Minor,
+				Number2: version2.Minor,
+			}, version_core.Block{
+				Name:    "patch",
+				Number1: version1.Patch,
+				Number2: version2.Patch,
+			},
+		)
+
+		compare := version_core.Compare(blocks)
+
+		fmt.Printf("%s\n", compare)
+
+	}
+
+	fmt.Printf("Error! Not enoght arguments.\n")
+
+}
