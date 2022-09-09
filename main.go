a78f8fb666312158ea7fd5298d276c303a0d02e6
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-09-09 13:31:55 +0200

    feat!: Add statement & command_line functions.

--- main.go
+++ main.go
@@ -2,89 +2,47 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"github.com/gowizzard/compver/v2/version_core"
-	"os"
+	"github.com/gowizzard/compver/v3/command_line"
+	"github.com/gowizzard/compver/v3/statement"
 )
 
+// compare is to save the boolean for the compare statement
 // version1, version2 is to save the version numbers from the flags
 var (
-	version1 *string
-	version2 *string
-	found    = Found{
-		Version1: false,
-		Version2: false,
-	}
+	compare            *bool
+	version1, version2 *string
+	visit              = make(map[string]bool)
 )
 
-// Found is to check if the flags are used
-type Found struct {
-	Version1 bool
-	Version2 bool
-}
-
 // init is to parse the versions from the flags
+// And check all visited flags
 func init() {
+
+	compare = flag.Bool("compare", false, "Set the statement to compare the version numbers")
 	version1 = flag.String("version1", "1.1.0", "Set the first version number")
 	version2 = flag.String("version2", "1.0.5", "Set the second version number")
 	flag.Parse()
+
+	flag.Visit(func(f *flag.Flag) {
+		visit[f.Name] = true
+	})
+
 }
 
 // main is to check the flags from the cli
-// Split the version core in major, minor & patch
-// And check the compare of the single blocks
+// And execute the statements or return the no statement message
 func main() {
 
-	flag.Visit(func(f *flag.Flag) {
-		switch f.Name {
-		case "version1":
-			found.Version1 = true
-		case "version2":
-			found.Version2 = true
-		}
-	})
+	if *compare && visit["version1"] && visit["version2"] {
 
-	if found.Version1 && found.Version2 {
-
-		core1, err := version_core.Split(*version1)
+		compare, err := statement.Compare(version1, version2)
 		if err != nil {
-			fmt.Printf("%s\n", err)
-			os.Exit(1)
+			command_line.Print(1, "%s\n", err)
 		}
-
-		core2, err := version_core.Split(*version2)
-		if err != nil {
-			fmt.Printf("%v\n", err)
-			os.Exit(1)
-		}
-
-		var blocks []version_core.Block
-		blocks = append(
-			blocks, version_core.Block{
-				Name:    "major",
-				Number1: core1.Major,
-				Number2: core2.Major,
-			}, version_core.Block{
-				Name:    "minor",
-				Number1: core1.Minor,
-				Number2: core2.Minor,
-			}, version_core.Block{
-				Name:    "patch",
-				Number1: core1.Patch,
-				Number2: core2.Patch,
-			},
-		)
-
-		compare := version_core.Compare(blocks)
-
-		fmt.Printf("%s\n", compare)
-		os.Exit(0)
-
-	} else {
-
-		fmt.Printf("Error! Not all required flags were specified.\n")
-		os.Exit(1)
+		command_line.Print(0, "%s\n", compare)
 
 	}
+
+	command_line.Print(1, "Error! No statement flags were specified.\n")
 
 }
