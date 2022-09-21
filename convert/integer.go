fdfc8626be0beff0694a40f47f3e72a4689b3514
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-09-21 16:30:27 +0200

    refactor: Add the copyright documentation.

--- convert/integer.go
+++ convert/integer.go
@@ -1,3 +1,7 @@
+// Copyright 2022 Jonas Kwiedor. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
 // Package convert contains functions with which the
 // type of value can be changed. If a value is not converted
 // due to an error, the default value of the type to be converted is used.
