d8aca27e7e21f74e50b92edb9be7e7afd9a9e1b3
Jonas Kwiedor <mail@gowizzard.de>
2022-10-04 07:34:20 +0200

    feat: Add prerelease & buildmetadata.

--- statement/core.go
+++ statement/core.go
@@ -7,23 +7,27 @@ package statement
 import "github.com/gowizzard/compver/v3/version_core"
 
 // Core is to get the version number, split it to core block and return them.
-func Core(version, block string) (int, error) {
+func Core(version, block string) (any, error) {
 
 	core, err := version_core.Split(version)
 	if err != nil {
 		return 0, err
 	}
 
-	var number int
+	var result any
 	switch block {
 	case "major":
-		number = core.Major
+		result = core.Major
 	case "minor":
-		number = core.Minor
+		result = core.Minor
 	case "patch":
-		number = core.Patch
+		result = core.Patch
+	case "prerelease":
+		result = core.Prerelease
+	case "buildmetadata":
+		result = core.Buildmetadata
 	}
 
-	return number, nil
+	return result, nil
 
 }
